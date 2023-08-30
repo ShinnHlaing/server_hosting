c78b731b0a14a7519aa49f664dea440b72494002
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-08-30 10:59:53 +0630

    db changes

--- admin/Model/dbConnection.php
+++ admin/Model/dbConnection.php
@@ -1,9 +1,9 @@
 <?php
-$hostname = "localhost";
-$port = "3306";
+$hostname = "db-mysql-sgp1-11888-do-user-14537762-0.b.db.ondigitalocean.com";
+$port = "25060";
 $dbname = "vr_carer";
-$username = "root";
-$password = "rootroot";
+$username = "doadmin";
+$password = "AVNS_eEZ24Wn4cRsjYinJQ_e";
 $pdo = new PDO(
     "mysql:host=$hostname;port=$port;dbname=$dbname",
     $username,
